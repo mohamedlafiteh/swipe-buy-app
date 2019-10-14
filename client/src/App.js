5fe8a91c606a86da31d8ccb32389f08215cf2972
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-10-14 18:35:40 +0100

    Add app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
-
+import Categories from "./components/Categories";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: 1,
+          model: "cs",
+          picture:
+            "https://vcache.arnoldclark.com/imageserver/AORSNFC3D6-CUS4/800/f(200 kB)https://vcache.arnoldclark.com/imageserver/AORSNFC3D6-CUS4/800/f"
+        },
+        {
+          id: 2,
+          model: "fg",
+          picture:
+            "https://vcache.arnoldclark.com/imageserver/CPCZ_GU3C6INISJ/800/f(90 kB)https://vcache.arnoldclark.com/imageserver/CPCZ_GU3C6INISJ/800/f"
+        },
+        {
+          id: 3,
+          model: "vb",
+          picture:
+            "https://vcache.arnoldclark.com/imageserver/ACRSNYB4F1-VUS3/350/f(15 kB)https://vcache.arnoldclark.com/imageserver/ACRSNYB4F1-VUS3/350/f"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>app</h1>
+        <Categories />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Categories.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Categories extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1> Categories</h1>
+      </div>
+    );
+  }
+}
+
+export default Categories;
